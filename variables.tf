3dc7b2a352940e0b9b83fbd0b1774e2c10e9bbc7
sunieldevarapu <sunil.devarapu1019@gmail.com>
2021-12-25 11:43:18 +0530

    New files added

--- variables.tf
+++ variables.tf
@@ -4,10 +4,10 @@ variable "aws_region" {}
 variable "amis" {
     description = "AMIs by region"
     default = {
-        us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
-		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
-		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
-		us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
+        us-east-1 = "ami-04505e74c0741db8d" # ubuntu 20.04 LTS
+		#us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
+		#us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
+		#us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
     }
 }
 variable "vpc_cidr" {}
@@ -20,8 +20,8 @@ variable "public_subnet3_cidr" {}
 variable "private_subnet_cidr" {}
 variable "public_subnet1_name" {}
 variable "public_subnet2_name" {}
-variable "public_subnet3_name" {}
-variable "private_subnet_name" {}
+#variable "public_subnet3_name" {}
+#variable "private_subnet_name" {}
 variable Main_Routing_Table {}
 variable "azs" {
   description = "Run the EC2 Instances in these Availability Zones"
