94ab1af8d9460933c57167399fc994d31672f9fd
sunieldevarapu <sunil.devarapu1019@gmail.com>
2021-12-25 11:25:31 +0530

    New files added

--- main.tf
+++ main.tf
@@ -1,20 +1,28 @@
 #This Terraform Code Deploys Basic VPC Infra.
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
+    #access_key = "${var.aws_access_key}"
+    #secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
 }
 
 terraform {
-  required_version = "<= 0.14" #Forcing which version of Terraform needs to be used
+  required_version = "<= 1.0.14" #Forcing which version of Terraform needs to be used
   required_providers {
     aws = {
-      version = "<= 3.0.0" #Forcing which version of plugin needs to be used.
+      version = "<= 4.0.0" #Forcing which version of plugin needs to be used.
       source = "hashicorp/aws"
     }
   }
 }
 
+terraform {
+    backend "s3" {
+      bucket = "orange048bucket"
+      key = "myterraform1.tfstate"
+      region = "us-east-1"
+    }
+    
+}
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
